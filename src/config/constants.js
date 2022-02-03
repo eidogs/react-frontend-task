032a42c01ee5c0a0dcfb63280677c0f5ea0eb911
eidogs <91258928+eidogs@users.noreply.github.com>
2022-02-03 22:14:07 +0100

    Changed json placeholder url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
 export const USERS_URL = 'https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data';
-export const JSON_PLACEHOLDER_URL = 'https://jsonplaceholder.typicode.com/'
+export const JSON_PLACEHOLDER_URL = 'https://jsonplaceholder.typicode.com/users/'
